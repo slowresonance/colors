c719a69522b96317d5416b93e0a2dd565c605e3e
mohanvpatta <mohanvpatta@gmail.com>
2022-05-17 01:04:38 +0530

    simplified contentscript

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,38 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@webcomponents/custom-elements";
 import ContentScript from "./ContentScript";
-import { StylesProvider, jssPreset } from "@material-ui/styles";
-import { create } from "jss";
 
-class ReactExtensionContainer extends HTMLElement {
-  connectedCallback() {
-    const mountPoint = document.createElement("span");
-    mountPoint.id = "reactExtensionPoint";
+const mount = document.createElement("span");
+const id = "colors-mount-point";
+mount.id = id;
+mount.style.display = "none";
+document.body.appendChild(mount);
 
-    const reactRoot = this.attachShadow({ mode: "open" }).appendChild(
-      mountPoint
-    );
-
-    const jss = create({
-      ...jssPreset(),
-      insertionPoint: reactRoot,
-    });
-
-    ReactDOM.render(
-      <StylesProvider jss={jss}>
-        <ContentScript />
-      </StylesProvider>,
-      mountPoint
-    );
-  }
-}
-
-const initWebComponent = function () {
-  customElements.define("react-extension-container", ReactExtensionContainer);
-
-  const app = document.createElement("react-extension-container");
-  document.documentElement.appendChild(app);
-};
-
-initWebComponent();
+ReactDOM.render(
+  <React.StrictMode>
+    <ContentScript />
+  </React.StrictMode>,
+  document.getElementById(id)
+);
